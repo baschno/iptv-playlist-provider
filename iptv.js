25e9812f9e93ebea8e97a092ea79beda5bef333a
Bastian Schnorbus <bastian.schnorbus@googlemail.com>
2023-11-01 00:04:44 +0100

    Added pathname to readFile

--- iptv.js
+++ iptv.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
         console.log('Received request for ' + req.url);
         res.writeHead(200);
-        fs.readFile('response.txt', function (error, content) {
+        fs.readFile('data/response.txt', function (error, content) {
                 res.end(content);
         })
 });
