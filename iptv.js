3a86c1bdfeaa9ddccaf2db0b940b02ac1510a3c8
baschno <bastian.schnorbus@gmail.com>
2023-11-01 10:10:05 +0100

    Add real playlist file, renaming

--- iptv.js
+++ iptv.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
         console.log('Received request for ' + req.url);
         res.writeHead(200);
-        fs.readFile('data/response.txt', function (error, content) {
+        fs.readFile('data/playlist.m3u', function (error, content) {
                 res.end(content);
         })
 });
